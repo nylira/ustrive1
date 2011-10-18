a7b085431526f90af7da5db685c69bd0fcc0b311
Peng Zhong <peng@nylira.com>
2011-10-18 09:53:26 +0200

    update arrow

--- j/script.js
+++ j/script.js
@@ -1,6 +1,9 @@
 $('a.compose').click(function (){
   $('#composition-form').toggleClass('visible');
 });
+$('span.more').click(function (){
+  $('ul.more').toggleClass('visible');
+});
 
 $('#folders').tabs({ fx: { height: 'toggle', opacity: 'toggle' } });
 
